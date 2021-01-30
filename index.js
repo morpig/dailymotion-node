861710270843f7ca938ffedcc672065f4f5525cd
Dito Windyaksa <morpig2@gmail.com>
2021-01-31 00:28:22 +0700

    ss

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const superagent = require('superagent')
+const youtubedl = require('youtube-dl')
+
+const app = express()
+
+let link = {};
+
+function fetchChannel() {
+
+}
+
+app.get("/dailymotion/:id", (req, res) => {
+    const url = 'https://www.dailymotion.com/embed/video/' + req.params.id
+    youtubedl.getInfo(url, function(err, info) {
+      if (err) {
+          res.status(500).send(err)
+          console.log(`${new Date()}: failed fetched ${req.params.id}`)
+      } else {
+          console.log(`${new Date()}: fetched ${req.params.id}, ${info.url}`)
+          res.redirect(info.url)
+      }
+    })
+});
+
+app.listen(process.env.PORT || 3000);
